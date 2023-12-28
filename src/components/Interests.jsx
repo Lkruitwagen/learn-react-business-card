8f7cddab38c2ab931bd32d426b6e3bf94b26f21f
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2023-12-28 05:14:26 +0000

    feat: components

---
+++ src/components/Interests.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+function Interests(){
+    return (
+        <div className="content--box">
+            <h3>
+                Interests
+            </h3>
+            <p>
+                Hiking, backpacking, and cycling; anything outdoors. 
+                George RR Martin, the hugo winners, solarpunk, and cli-fi.
+            </p>
+        </div>
+    )
+}
+
+export default Interests
